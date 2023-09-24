a37045c375bf6627e1048a1ffb531d32b2d75562
KaiqueSlasher <142070574+KaiqueSlasher@users.noreply.github.com>
2023-09-24 19:17:45 -0300

    Add files via upload

---
+++ estudo dirigido/exercicio_quantasvezesletraa.py
@@ -0,0 +1,11 @@
+texto_exemplo = "Aqui vai um texto arbitrário qualquer com algumas para não dizer várias letras a para contar quantas vezes a letra a aparece alternadamente ou antes ou a posteriori."
+
+def contagem_de_a(texto):
+    contagem = 0
+    for letra in texto: 
+        if letra.lower() == "a":
+            contagem += 1
+    return contagem
+
+print(texto_exemplo)
+print(f"O texto contém a letra 'a' {contagem_de_a(texto_exemplo)} vezes.")
